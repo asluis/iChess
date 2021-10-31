5a9724f42579907d656771f7bd1f7952dbcfcb92
asluis <asluisinfo@gmail.com>
2021-10-30 19:41:18 -0700

    Reformatting and adding comments

--- src/application/StorageManager.java
+++ src/application/StorageManager.java
@@ -7,18 +7,20 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * StorageManager is a class designed to manage iChess data within .txt files. The data is expected to be comma separated values (csv).
+ * StorageManager is a class designed to manage iChess data within .txt files.
+ * The data is expected to be comma separated values (csv).
+ * 
  * @author Luis Alvarez Sanchez
  */
 public class StorageManager {
 	private static final int USER_NUM_ATTRIBUTES = 4; // username, password, wins, losses
 	private static final String DEFAULT_FILE_NAME = "datastore.txt";
-	
+
 	private String filename;
 	private int numItemsPerRow;
-	
+
 	/**
-	 * Instantiates a StorageManager with the default filename. 
+	 * Instantiates a StorageManager with the default filename.
 	 */
 	public StorageManager() {
 		filename = DEFAULT_FILE_NAME;
@@ -26,17 +28,20 @@ public class StorageManager {
 	}
 
 	/**
-	 * Instantiates StorageManager with a filename. 
-	 * @param filename size > 0. Should include .txt
+	 * Instantiates StorageManager with a filename.
+	 * 
+	 * @param filename       size > 0. Should include .txt
 	 * @param numItemsPerRow must be greater than zero or the default will be used
 	 */
 	public StorageManager(String filename, int numItemsPerRow) {
 		this.filename = filename.length() > 0 ? filename : DEFAULT_FILE_NAME;
 		this.numItemsPerRow = numItemsPerRow > 0 ? numItemsPerRow : USER_NUM_ATTRIBUTES;
 	}
-	
+
 	/**
-	 * Returns a scanner loaded with the entire file.
+	 * Returns a scanner loaded with the entire file. This is how each line will be
+	 * read from the file.
+	 * 
 	 * @return scanner if successful, null if not possible
 	 */
 	public Scanner getScanner() {
@@ -49,33 +54,33 @@ public class StorageManager {
 
 	/**
 	 * writes values to the current file storage. Must be one line at a time
+	 * 
 	 * @param str - data to be written
 	 * @return true if successful, false if not
 	 */
 	public boolean write(String str) {
-		if(!isValidEntry(str)) return false; // invalid entry
-		
-		
+		if (!isValidEntry(str))
+			return false; // invalid entry
+
 		try {
 			FileWriter writer = new FileWriter(filename);
 			writer.write(str + "\n"); // appending \n per write to ensure one entry per line in .txt file
-			
+
 			writer.close();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			return false;
 		}
 		return true;
 	}
-	
+
 	/**
-	 * Ensures values entered are in csv format.
-	 * Expected format:
-	 * 		- Values separated by comma
-	 * 		- Number of values entered matches the expected amount
+	 * Ensures values entered are in csv format. Expected format: - Values separated
+	 * by comma - Number of values entered matches the expected amount
+	 * 
 	 * @param str
-	 * @return	true if valid, false if not
+	 * @return true if valid, false if not
 	 */
 	private boolean isValidEntry(String str) {
 		return str.split(",").length == numItemsPerRow;
