2ab9a6f0c920de2e51b428c82dfa27687b72348d
asluis <asluisinfo@gmail.com>
2021-10-30 23:37:40 -0700

    Developed the controller more and modified main to use the controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,29 +1,19 @@
 package application;
 
-import UserInterface.LoginMenu;
 import controller.Controller;
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		
-		Controller ctrl = new Controller();
+		primaryStage.setTitle("iChess");
+		primaryStage.setHeight(1000);
+		primaryStage.setWidth(1000);
 		
-		try {
-			primaryStage.setTitle("iChess");
-			primaryStage.setHeight(1000);
-			primaryStage.setWidth(1000);
-			Scene menuScene = new Scene(new LoginMenu());
-			menuScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
-			primaryStage.setScene(menuScene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		Controller ctrl = new Controller(primaryStage);
+		ctrl.start();
 	}
 	
 	public static void main(String[] args) {
