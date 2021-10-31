fa3f76bd747f72fa95f050a6fd0ea8a2112bc3b2
asluis <asluisinfo@gmail.com>
2021-10-30 17:29:30 -0700

    Fixed createFile() method within StorageManager. TODO: write and read methods

--- src/application/StorageManager.java
+++ src/application/StorageManager.java
@@ -1,5 +1,50 @@
 package application;
 
-public class StorageManager {
+import java.io.File;
 
+public class StorageManager {
+	private String fileName;
+	
+	/**
+	 * Instantiates a StorageManager with the default filename.
+	 */
+	public StorageManager() {
+		fileName = "datastore.txt";
+	}
+	
+	/**
+	 * Instantiates StorageManager with a fileName. 
+	 * @param fileName
+	 */
+	public StorageManager(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	
+	
+	/**
+	 * Creates a file.
+	 * @return true if file created successfully, false if file already exists.
+	 * @throws Exception 
+	 */
+	public boolean createFile() throws Exception{
+		File file = new File(this.fileName);
+		return file.createNewFile();
+	}
+	
+	/**
+	 * Gets the filename the StorageManager is handling
+	 * @return
+	 */
+	protected String getFileName() {
+		return fileName;
+	}
+	
+	/**
+	 * Changes the file the StorageManager is handling.
+	 * @param fileName
+	 */
+	protected void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
 }
